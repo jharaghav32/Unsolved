c95180569f9c208097dbfaaad09f06c374336bd0
jharaghav32 <jharaghav3209@gmail.com>
2022-08-16 23:10:42 +0530

    recover tree from preorder serialized string

---
+++ BinaryTree/recovertree.cpp
@@ -0,0 +1,40 @@
+// // problem link:https://leetcode.com/problems/recover-a-tree-from-preorder-traversal/submissions/
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     TreeNode* recoverFromPreorder(string traversal) {
+//         unordered_map<int,TreeNode*>ump;
+//         int dash=0;
+//         for(int i=0;i<traversal.size();i++){
+//             if(traversal[i]=='-')dash++;
+//             else{
+//                  string ans="";
+//                 int start=i;
+//                 while(i<traversal.size()&& isdigit(traversal[i]))
+//                   i++;
+//                 i--;
+                
+//                 int value = stoi(traversal.substr(start,i-start+1));
+//                 ump[dash]= new TreeNode(value);
+//                 if(dash){
+//                     if(ump[dash-1]->left)
+//                         ump[dash-1]->right = ump[dash];
+//                     else
+//                         ump[dash-1]->left=ump[dash];
+//                     dash=0;
+//                 }
+//             }
+//         }
+//         return ump[0];
+//     }
+// };
